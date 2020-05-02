c352f8adfc9fd50e14ce3b5bd7a801370eef938a
Daksh Paleria <57083450+dakshp07@users.noreply.github.com>
2020-05-02 17:10:50 +0530

    Add files via upload

---
+++ calc.js
@@ -0,0 +1,22 @@
+$(".sub").on("click",function even(){
+    if($(".fi").val()=="" &&  $(".se").val()=="")
+    {
+        alert("Please Enter The Numbers");
+    }
+    else{
+        var num1=$(".fi").val();
+        var num2=$(".se").val();
+        var sum=parseInt(num1)+parseInt(num2);
+        var diff=parseInt(num1)-parseInt(num2);
+        var pro=parseInt(num1)*parseInt(num2);
+        var div=parseFloat(num1)/parseFloat(num2);
+        $(".te").val(sum);
+        $(".fr").val(diff);
+        $(".fif").val(pro);
+        $(".si").val(div);
+    }
+    
+});
+$(".clr").on("click",function odd(){
+    $(".fi").val("") &&  $(".se").val("");
+});
