76a442d8e7f2bd9c2f8f096a27971b62ec5a3ac7
Daksh Paleria <57083450+dakshp07@users.noreply.github.com>
2020-05-02 17:24:34 +0530

    Update calc.js

--- calc.js
+++ calc.js
@@ -18,5 +18,5 @@ $(".sub").on("click",function even(){
     
 });
 $(".clr").on("click",function odd(){
-    $(".fi").val("") &&  $(".se").val("") && $(".te").val("") && $(".fr").val("") && $(".fif").val("") && $(".si").val("");
+    location.reload();
 });
